80adbdafcb6d83b7715ae768a4fb30f40c44c40f
MrLucky0307 <iliya-karyalaynen@yandex.ru>
2023-02-06 15:29:13 +0300

    Initial commit

---
+++ Итераторы, генераторы/task_1.py
@@ -0,0 +1,41 @@
+class FlatIterator:
+
+    def __init__(self, list_of_list):
+        self.start = -1
+        self.list = []
+        for i in list_of_list:
+            self.list += i
+        self.end = len(self.list)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.start += 1
+        if self.start == self.end:
+            raise StopIteration
+        for item in self.list:
+            self.list.remove(item)
+            return item
+
+
+def test_1():
+
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            FlatIterator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+
+if __name__ == '__main__':
+    test_1()
